8f3b8f2410ab26b03d207c0198c149c266e4f1c1
Lexi <sashabelana@gmail.com>
2026-01-15 17:28:59 -0300

    Ejercicio #00
    Añadí una carpeta para el ejercicio #00, y un archivo para el enunciado y otro para la resolución

---
+++ Practicas con la lesi/codigo_prueba_1.py
@@ -0,0 +1,6 @@
+numero_1 = 2
+numero_2 = 2
+
+resultado = numero_1 + numero_2
+
+print(resultado)
---
+++ Retos de Programación/#00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/#00 Ejercicio.py
@@ -0,0 +1,24 @@
+#https://www.python.org/
+
+
+#comentario de una línea
+
+"""
+comentario de
+varias líneas
+"""
+
+
+variable = 294
+#constante -> no existen en Python
+MY_CONSTANT = "constante" #por convención, nombres de constantes en mayúsculas
+
+
+a = 4 #int (integer/entero)
+b = 2.5 #float (floating point number/decimal)
+c = 6j #complex (número complejo, "j" es la parte imaginaria)
+d = "Lexi" #string (cadena de texto, comillas 'simples' o "dobles")
+e = True #boolean (solo True/verdadero o False/falso)
+
+
+print("¡Hola, Python!")
